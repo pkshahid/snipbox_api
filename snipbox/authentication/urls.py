f19238f51d290ec3df79a88a7f2014abd30ffb35
Shahid@Galaxy <shahidmohammedmsp@gmail.com>
2025-02-17 00:22:51 +0530

    Implemented a refresh token endpoint for access token refresh mechanism

--- snipbox/authentication/urls.py
+++ snipbox/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from authentication.views import RegisterView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
     path('login', TokenObtainPairView.as_view(), name="login"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
