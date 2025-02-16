0d2db1d6833e9d203851cd1d9f5abb65e3ddffb1
Shahid@Galaxy <shahidmohammedmsp@gmail.com>
2025-02-17 00:20:47 +0530

    Created a Login endpoint to authenticate users and return an access token

--- snipbox/authentication/urls.py
+++ snipbox/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from authentication.views import RegisterView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
+    path('login', TokenObtainPairView.as_view(), name="login"),
 ]
